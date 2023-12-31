4c89da305633dcf070522819e0b0c598f04ddd1a
PLMohamed <141277277+PLMohamed@users.noreply.github.com>
2023-12-31 17:37:59 +0100

    Create Main Window

--- app/index.js
+++ app/index.js
@@ -1,1 +1,41 @@
 "use strict"
+
+const { app , BrowserWindow , Menu} = require('electron');
+const path = require('path');
+const gotTheLock = app.requestSingleInstanceLock();
+let mainWindow;
+
+// Make sure only one app at the time
+if (!gotTheLock) {
+    app.quit();
+    return;
+  }
+
+const createWindow = () => {
+    mainWindow = new BrowserWindow({
+        width:800,
+        height:600,
+        webPreferences: {
+            preload : path.join(__dirname,'preload.js'),
+            nodeIntegration: false,
+            contextIsolation: true,
+        }
+    });
+
+    mainWindow.loadFile(path.join(__dirname,'src/index.html'));
+
+    mainWindow.on('ready-to-show',mainWindow.show)
+
+}
+
+app.on('ready',createWindow);
+
+app.on('activate',() => {
+    if(BrowserWindow.getAllWindows().length === 0)
+        createWindow();
+});
+
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
+  })
