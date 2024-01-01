31152bcba237af0eee0cde5d1476aff36c2b3198
PLMohamed <141277277+PLMohamed@users.noreply.github.com>
2024-01-01 20:46:01 +0100

    Added Surahs Made every Ayah display in line Added Function to add surah in cookie 'doesnt work on big data'

---
+++ app/src/public/js/surah.js
@@ -0,0 +1,65 @@
+"use strict";
+
+document.addEventListener('DOMContentLoaded',async () => {
+    const surahID = window.location.href.split('/').pop();
+    const surahTitle = document.getElementById('surahTitle');
+    const surahContainer = document.querySelector('.container');
+    const bsm = document.getElementById('bsm'); 
+    const response = await fetch(`http://api.alquran.cloud/v1/surah/${surahID}`,{
+        "method":"GET"
+    });
+    try {
+        const result = await response.json();
+        if(result.status !== "OK")
+            throw new Error(`Error ${result.code}`);
+        
+        display(result);
+        setMultipleCookies(JSON.stringify(result),result.data.englishName)
+
+        
+    } catch (err) {
+        console.error(err);
+
+    }
+
+    function display(result) {
+        const data = result.data;
+        const ayahs = data.ayahs;
+
+        
+        surahTitle.innerHTML = data.name;
+        document.querySelector('title').innerHTML = data.name;
+
+        ayahs.forEach((ayah,index) => {
+            if(index === 0 && surahID != 9){
+                bsm.innerHTML = "بِسۡمِ ٱللَّهِ ٱلرَّحۡمَـٰنِ ٱلرَّحِیمِ";
+                displayAyah(ayah.text.split("بِسۡمِ ٱللَّهِ ٱلرَّحۡمَـٰنِ ٱلرَّحِیمِ")[1]);
+            }
+            else
+                displayAyah(ayah.text);
+            
+        });
+    }
+
+    function displayAyah(ayah) {
+        if(!ayah) return;
+        const ayahContainer = document.createElement('p');
+        ayahContainer.innerHTML = ayah;
+        surahContainer.appendChild(ayahContainer);
+    }
+
+    // Function to set multiple cookies from a string
+    function setMultipleCookies(dataString, prefix, maxCookieSize = 4090) {
+        const dataSize = dataString.length;
+        const numCookies = Math.ceil(dataSize / maxCookieSize);
+
+        for (let i = 0; i < numCookies; i++) {
+            const start = i * maxCookieSize;
+            const end = (i + 1) * maxCookieSize;
+            const cookieData = dataString.substring(start, end);
+
+            // Set each cookie with a unique name (using a prefix) and the corresponding data
+            document.cookie = `${prefix}${i}=${encodeURIComponent(cookieData)}`;
+        }
+    }
+})
